b11c0a458cf899ae1254fc153121eb7949888b9e
Rajeev Enugu <rrajeevenugu@gmail.com>
2025-09-26 09:26:20 +0530

    Create inheritance.cpp

---
+++ inheritance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class library {
+public:
+    library() {
+        cout << "Here is the library\n";
+    }
+};
+
+class book : private library {
+public:
+    string title;
+    string author;
+
+    book(string t, string a) {
+        title = t;
+        author = a;
+    }
+
+    void display() {
+        cout << "Book Title: " << title << endl;
+        cout << "Author: " << author << endl;
+    }
+};
+
+int main() {
+    book obj("Dairy of a wimpy kid","Greg Heffley");
+    obj.display();
+    return 0;
+}
+
