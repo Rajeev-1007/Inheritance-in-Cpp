3c4446fe341eb7ad8ab0cbb22f4f33e7db520598
Rajeev Enugu <rrajeevenugu@gmail.com>
2025-09-26 09:27:59 +0530

    Create multiple inheritance.cpp

---
+++ multiple inheritance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Library {
+public:
+    void myLibrary() {
+        cout << "This is the Library" << endl;
+    }
+};
+
+class Author {
+public:
+    void myAuthor() {
+        cout << "Author: Greg Heffley" << endl;
+    }
+};
+
+class Book : public Library, public Author {
+public:
+    void myBook() {
+        cout << "Book: Diary of a Wimpy Kid" << endl;
+    }
+};
+
+int main() {
+    Book obj;
+    obj.myBook();
+    obj.myAuthor();
+    obj.myLibrary();
+    return 0;
+}
